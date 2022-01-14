e4df34292d2e4f438b50eb0bec38431018ba043b
Hacker T <ttt9675@gmail.com>
2022-01-14 06:30:08 +0400

    Created edited and added new line

--- scr/53_1_1.cpp
+++ scr/53_1_1.cpp
@@ -1,5 +1,22 @@
-#include <iosteam>
+#include <iostream>
 using namespace std;
+
+class MyClass{
+	public:
+	myClass(){
+		cout<<"?Hey!";
+		}
+		void setName(string x){
+			name=x;
+			}
+			string getName(){
+				return name;
+				}
+				private:
+				string name;
+				};
 int main(){
+	myClass myObj;
+	
 	return 0;
 	}
